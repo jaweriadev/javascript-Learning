3eaa213cf4de32a0a970571023212003a5470250
jaweriadev <jaweriaabdulkhaliq@gmail.com>
2023-05-08 16:28:52 +0500

    more-learning

--- script.js
+++ script.js
@@ -26,3 +26,54 @@ console.log(letters.join("-"));
 // at method
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 console.log(fruits.at(2));
+
+// looping arrays for each loop
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// for loop
+for (const movement of movements) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log("-----ForEach------");
+
+movements.forEach(function (movement) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+});
+
+// another example of for each loop
+const numbers = [20, 10, 30, 12, 5];
+numbers.forEach(myFunction);
+function myFunction(item, index, arr) {
+  console.log((arr[index] = item * 10));
+}
+// another example of forEach loop
+
+var names = ["jaweria", "laviza", "uswah", "azwa"];
+names.forEach(function (value, index) {
+  console.log(index + " : " + value);
+});
+console.log("------refreshments-----");
+// another example of forEach loop
+var refreshment = ["Pasta", "Juice", "Lays", "kiwi"];
+refreshment.forEach(function (items, index) {
+  console.log(index + " : " + items);
+});
+
+// map
+const currencies = new Map([
+  ["USD", "United states dollar"],
+  ["EUR", "Euro"],
+  ["GBP", "Pound sterling"],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key} : ${value}`);
+});
